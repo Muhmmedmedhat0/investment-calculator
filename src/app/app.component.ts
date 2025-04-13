b68e73700a20b22829755888ae09cf36a9daf52a
muhmmad_medhat <m.medhat@zinad.net>
2025-04-14 00:14:00 +0200

    feat: refactor all components to use signals for state management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { type InvestmentInput } from '../investment-input.types';
@@ -14,7 +14,7 @@ import { type InvestmentResults } from './investment-results/investment-results.
 export class AppComponent {
   title = 'investment-calculator';
 
-  resusultsData?: InvestmentResults;
+  resusultsData = signal<InvestmentResults | undefined>(undefined);
 
   onCalculateInvestmentResults({
     initialInvestment,
@@ -42,6 +42,6 @@ export class AppComponent {
     }
 
     console.log(annualData);
-    this.resusultsData = annualData;
+    this.resusultsData.set(annualData);
   }
 }
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { type InvestmentInput } from '../../investment-input.types';
 import { FormsModule } from '@angular/forms';
 
@@ -9,19 +9,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  initialInvestment = '0';
-  annualInvestment = '0';
-  expectedReturn = '0';
-  duration = '0';
+  initialInvestment = signal('0');
+  annualInvestment = signal('0');
+  expectedReturn = signal('5');
+  duration = signal('10');
 
-  @Output() calculate = new EventEmitter<InvestmentInput>();
+  calculate = output<InvestmentInput>();
 
   onSubmit() {
     this.calculate.emit({
-      initialInvestment: +this.initialInvestment,
-      annualInvestment: +this.annualInvestment,
-      expectedReturn: +this.expectedReturn,
-      duration: +this.duration,
+      initialInvestment: +this.initialInvestment(),
+      annualInvestment: +this.annualInvestment(),
+      expectedReturn: +this.expectedReturn(),
+      duration: +this.duration(),
     });
+    this.initialInvestment.set('0');
+    this.annualInvestment.set('0');
+    this.expectedReturn.set('5');
+    this.duration.set('10');
   }
 }
